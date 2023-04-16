f0ed9671fc366743af36d22f6416e7656e1e5820
Luis Cuellar <luisenri2901@gmail.com>
2023-04-15 21:59:51 -0500

    Se implementan las List/Array en java, los métodos add, remove, set. Además se muestran 3 formas de leer un array: for, forEach y forEach con lambda. Por último, se muestran las diferentes formas como podemos ordenar una lista, haciendo uso de la libreria de Java Collections, implementando el .sort en la lista y usando el método en Stream

--- src/Colletions/Ejemplo1ArrayList.java
+++ src/Colletions/Ejemplo1ArrayList.java
@@ -1,6 +1,7 @@
 package Colletions;
 
-import java.util.ArrayList;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Ejemplo1ArrayList {
     public static void main(String[] args) {
@@ -11,11 +12,70 @@ public class Ejemplo1ArrayList {
 
         //Implemntacion de un array vacio de strings
         ArrayList<String> lista = new ArrayList<>();
-        lista.add(variable1);
+
+        //Añadir
+        lista.add(variable4);
         lista.add(variable2);
         lista.add(variable3);
-        lista.add(variable4);
+        lista.add(variable1);
+        lista.add(variable2);
 
         System.out.println(lista);
+
+        //Eliminar
+        lista.remove(2);
+        System.out.println(lista);
+
+        //Modificar
+        lista.set(2,"Ejemplo alterado");
+        System.out.println(lista);
+
+        //Ver tamaño de lista
+        System.out.println(lista.size());
+
+        //Leer Arrays-List
+
+        for(int i = 0; i < lista.size(); i++){
+            System.out.println(lista.get(i));
+        }
+
+        //forEach
+        for (String ejemplo: lista) {
+            System.out.println(ejemplo);
+        }
+
+        //forEach recurso de lambdas
+        lista.forEach(ejemplo ->{
+            System.out.println(ejemplo);
+        });
+
+        //Ordenar lista de Ascendente
+        Collections.sort(lista);
+        System.out.println(lista);
+
+        //Ordenar lista de Descendente
+        Collections.sort(lista, Collections.reverseOrder());
+        System.out.println(lista);
+
+        //Ordenar sin la librería Colletions
+        //Forma Ascendente
+        lista.sort(Comparator.naturalOrder());
+        System.out.println(lista);
+
+        //Forma Descendente
+        lista.sort(Comparator.reverseOrder());
+        System.out.println(lista);
+
+        //Ordenar por medio de Stream
+        //Stream crea una nueva lista
+        //Forma Ascendente
+        List<String> listaAsce = lista.stream().sorted().collect(Collectors.toList());
+        System.out.println(listaAsce);
+
+        //Forma Descendente
+        List<String> listaDesc = lista.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+        System.out.println(listaDesc);
+
+
     }
 }
